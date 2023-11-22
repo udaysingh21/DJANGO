b9fcbed41aff5624a6b84d1af95b5a40cce7f749
uday <uvs2111@gmail.com>
2023-11-22 09:31:19 +0530

    initial commit

--- my_video_rental/myvideo/admin.py
+++ my_video_rental/myvideo/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    # change the ordering of fields
+    fields=['release_year','title','length']
+
+    # search bar
+    search_fields=['title','length']
+
+    # filter
+    list_filter=['release_year','length']
+
+    # list display
+    list_display=['title','release_year','length']
+
+    # list editable - first display then edit otherwise error
+    list_editable=['length']
+
+
+admin.site.register(models.Movie,MovieAdmin)
 admin.site.register(models.Customer)
+
